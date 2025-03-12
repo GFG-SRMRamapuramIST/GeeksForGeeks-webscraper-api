9a36442af9f10f00132dc61b23c004dd5e24f755
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-03-12 12:15:24 +0530

    Update Playwright dependencies and refactor web scraping utility to use playwright-aws-lambda

--- utility/getHTMLPuppeteer.js
+++ utility/getHTMLPuppeteer.js
@@ -1,16 +1,19 @@
-const { chromium } = require("playwright");
+const playwright = require("playwright-aws-lambda");
 
 const getHTMLusingPlaywright = async function (url) {
-  const browser = await chromium.launch({
-    headless: true, // Headless mode (no UI)
-  });
-
+  const browser = await playwright.launchChromium({ headless: true });
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle" });
 
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+  );
+
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  // Get full HTML after JavaScript execution
   const html = await page.content();
-  await browser.close();
 
+  await browser.close();
   return html;
 };
 
