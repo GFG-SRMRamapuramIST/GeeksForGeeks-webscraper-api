c002d0f98057424b662223487ebe5d4000add3e7
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-03-12 11:18:15 +0530

    Enhance Puppeteer configuration and update page navigation strategy

--- utility/getHTMLPuppeteer.js
+++ utility/getHTMLPuppeteer.js
@@ -1,14 +1,20 @@
 const puppeteer = require("puppeteer");
 
 const getHTMLusingPuppeteer = async function (url) {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome-stable",
+  });
+
   const page = await browser.newPage();
 
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
   );
 
-  await page.goto(url, { waitUntil: "networkidle2" });
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
   // Get full HTML after JavaScript execution
   const html = await page.content();
