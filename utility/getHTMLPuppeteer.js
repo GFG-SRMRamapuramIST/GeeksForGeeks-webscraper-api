1b3e4a13e941f76437cc56893aed04e7ec675fbf
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-03-12 12:19:25 +0530

    Refactor web scraping utility to improve page scraping logic and remove unnecessary postinstall script

--- utility/getHTMLPuppeteer.js
+++ utility/getHTMLPuppeteer.js
@@ -1,20 +1,16 @@
 const playwright = require("playwright-aws-lambda");
 
-const getHTMLusingPlaywright = async function (url) {
+async function scrapePage(url) {
   const browser = await playwright.launchChromium({ headless: true });
-  const page = await browser.newPage();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-  );
+  await page.goto(url, { waitUntil: "networkidle" });
 
-  await page.goto(url, { waitUntil: "networkidle2" });
-
-  // Get full HTML after JavaScript execution
-  const html = await page.content();
+  const data = await page.evaluate(() => {
+    return document.querySelector("body").innerText;
+  });
 
   await browser.close();
-  return html;
-};
-
-module.exports = getHTMLusingPlaywright;
+  return data;
+}
