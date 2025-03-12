913ac542828e37910e3d5b39cfdced1f0c98edc7
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-03-12 12:23:25 +0530

    Refactor web scraping utility to rename function and streamline data extraction

--- utility/getHTMLPuppeteer.js
+++ utility/getHTMLPuppeteer.js
@@ -1,16 +1,16 @@
 const playwright = require("playwright-aws-lambda");
 
-async function scrapePage(url) {
+async function getHTMLusingPlaywright(url) {
   const browser = await playwright.launchChromium({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
   await page.goto(url, { waitUntil: "networkidle" });
 
-  const data = await page.evaluate(() => {
-    return document.querySelector("body").innerText;
-  });
+  const data = await page.evaluate(() => document.body.innerText);
 
   await browser.close();
   return data;
 }
+
+module.exports = { getHTMLusingPlaywright };
