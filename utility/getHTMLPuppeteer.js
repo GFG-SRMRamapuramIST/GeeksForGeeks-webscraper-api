2a49776237c4221cf7586454a62887873a03e70f
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-03-12 11:53:12 +0530

    Replace Puppeteer with Puppeteer-Core and update browser launch configuration

--- utility/getHTMLPuppeteer.js
+++ utility/getHTMLPuppeteer.js
@@ -1,22 +1,20 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 const getHTMLusingPuppeteer = async function (url) {
   const browser = await puppeteer.launch({
-    headless: "new",
+    executablePath: "/usr/bin/chromium-browser",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
   });
 
   const page = await browser.newPage();
-
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
   );
 
   await page.goto(url, { waitUntil: "domcontentloaded" });
 
-  // Get full HTML after JavaScript execution
   const html = await page.content();
-
   await browser.close();
   return html;
 };
